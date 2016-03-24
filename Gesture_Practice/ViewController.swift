046390abb4b98c5cd5d3e06d6a80e0b81bcff068
Johnson Kaho Poon <kahopoon@gmail.com>
2016-03-24 20:06:27 +0800

    add up and down swipe programmatically

--- Gesture_Practice/ViewController.swift
+++ Gesture_Practice/ViewController.swift
@@ -18,8 +18,26 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         imageView.image = UIImage(named: imageArray.first!)
+        
+        let swipeUp = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.upSwipe))
+        swipeUp.direction = UISwipeGestureRecognizerDirection.Up
+        self.view.addGestureRecognizer(swipeUp)
+        
+        let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.downSwipe))
+        swipeDown.direction = UISwipeGestureRecognizerDirection.Down
+        self.view.addGestureRecognizer(swipeDown)
     }
 
+    func upSwipe() {
+        imageView.image = UIImage(named: imageArray[0])
+        currentImage = 0
+    }
+    
+    func downSwipe() {
+        imageView.image = UIImage(named: imageArray.last!)
+        currentImage = imageArray.indexOf(imageArray.last!)!
+    }
+    
     @IBAction func rightSwipe(sender: AnyObject) {
         let updateShow = currentImage + 1 < imageArray.count ? currentImage + 1 : 0
         swipeAction(updateShow)
